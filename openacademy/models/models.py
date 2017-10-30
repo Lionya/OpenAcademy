381633103c87ef6901ffcdd03ad56456d21ae967
Lionya <lion.ya@gmail.com>
2017-10-30 22:58:21 +0200

    Tranclate end

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-
 from datetime import timedelta
-from odoo import models, fields, api, exceptions
+from odoo import models, fields, api, exceptions, _
 
 
 class Course(models.Model):
@@ -11,23 +10,10 @@ class Course(models.Model):
     description = fields.Text()
 
     responsible_id = fields.Many2one('res.users',
-        ondelete='set null', string="Responsible", index=True)
+                                     ondelete='set null', string="Responsible",
+                                     index=True)
     session_ids = fields.One2many(
         'openacademy.session', 'course_id', string="Sessions")
-
-    @api.multi
-    def copy(self, default=None):
-        default = dict(default or {})
-
-        copied_count = self.search_count(
-            [('name', '=like', u"Copy of {}%".format(self.name))])
-        if not copied_count:
-            new_name = u"Copy of {}".format(self.name)
-        else:
-            new_name = u"Copy of {} ({})".format(self.name, copied_count)
-
-        default['name'] = new_name
-        return super(Course, self).copy(default)
 
     _sql_constraints = [
         ('name_description_check',
@@ -39,29 +25,42 @@ class Course(models.Model):
          "The course title must be unique"),
     ]
 
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', _(u"Copy of {}%").format(self.name))])
+        if not copied_count:
+            new_name = _(u"Copy of {}").format(self.name)
+        else:
+            new_name = _(u"Copy of {} ({})").format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+
 
 class Session(models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(required=True)
     start_date = fields.Date(default=fields.Date.today)
-    duration = fields.Float(digits=(6, 2), help="Duration in days")
-    seats = fields.Integer(string="Number of seats")
+    duration = fields.Float(digits=(6, 2), help='Duration in days')
+    seats = fields.Integer(string='Number of seats')
     active = fields.Boolean(default=True)
     color = fields.Integer()
 
     instructor_id = fields.Many2one('res.partner', string="Instructor",
-    domain = ['|', ('instructor', '=', True),
-              ('category_id.name', 'ilike', "Teacher")])
+                                    domain=['|', ('instructor', '=', True),
+                                            ('category_id.name', 'ilike',
+                                             "Teacher")])
     course_id = fields.Many2one('openacademy.course',
-        ondelete='cascade', string="Course", required=True)
+                                ondelete='cascade', string="Course",
+                                required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
-
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
     end_date = fields.Date(string="End Date", store=True,
-        compute='_get_end_date', inverse='_set_end_date')
-    hours = fields.Float(string="Duration in hours",
-                         compute='_get_hours', inverse='_set_hours')
+                           compute='_get_end_date', inverse='_set_end_date')
     attendees_count = fields.Integer(
         string="Attendees count", compute='_get_attendees_count', store=True)
 
@@ -89,15 +88,16 @@ class Session(models.Model):
             if not r.seats:
                 r.taken_seats = 0.0
             else:
-                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+                r.taken_seats = 100 * len(r.attendee_ids) / r.seats
 
     @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
         if self.seats < 0:
             return {
                 'warning': {
-                    'title': "Incorrect 'seats' value",
-                    'message': "The number of available seats may not be negative",
+                    'title': _("Incorrect 'seats' value"),
+                    'message': _("The number of available seats may "
+                               "not be negative"),
                 },
             }
         if self.seats < len(self.attendee_ids):
@@ -107,6 +107,18 @@ class Session(models.Model):
                     'message': "Increase seats or remove excess attendees",
                 },
             }
+
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        for r in self:
+            r.attendees_count = len(r.attendee_ids)
+
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError(
+                    _("A session's instructor can't be an attendee"))
 
     @api.depends('start_date', 'duration')
     def _get_end_date(self):
@@ -126,28 +138,9 @@ class Session(models.Model):
             if not (r.start_date and r.end_date):
                 continue
 
-            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # Compute the difference between dates, but:
+            # Friday - Monday = 4 days,
             # so add one day to get 5 days instead
             start_date = fields.Datetime.from_string(r.start_date)
             end_date = fields.Datetime.from_string(r.end_date)
             r.duration = (end_date - start_date).days + 1
-
-    @api.depends('duration')
-    def _get_hours(self):
-        for r in self:
-            r.hours = r.duration * 24
-
-    def _set_hours(self):
-        for r in self:
-            r.duration = r.hours / 24
-
-    @api.depends('attendee_ids')
-    def _get_attendees_count(self):
-        for r in self:
-            r.attendees_count = len(r.attendee_ids)
-
-    @api.constrains('instructor_id', 'attendee_ids')
-    def _check_instructor_not_in_attendees(self):
-        for r in self:
-            if r.instructor_id and r.instructor_id in r.attendee_ids:
-                raise exceptions.ValidationError("A session's instructor can't be an attendee")
